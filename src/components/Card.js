22c9875d640dbd8ef4c38b94c2353c8038030597
anagararna <anagararna@gmail.com>
2023-05-02 12:22:12 +0200

    listo

--- src/components/Card.js
+++ src/components/Card.js
@@ -12,6 +12,14 @@ const Card = ({loadingData, showData, weather, forecast }) => {
     var url = "";
     var iconUrl = "";
 
+    var iconUrl3 = "";
+    var iconUrl6 = "";
+    var iconUrl9 = "";
+
+    var forecastDate3 = "";
+    var forecastDate6 = "";
+    var forecastDate9 = "";
+
     if(loadingData){
     return <Spiner />;
 
@@ -19,6 +27,14 @@ const Card = ({loadingData, showData, weather, forecast }) => {
     if(showData) {
         url = "http://openweathermap.org/img/w/";
         iconUrl = url + weather.weather[0].icon + ".png";
+
+        iconUrl3 = url + forecast.list[1].weather[0].icon + ".png";
+        iconUrl6 = url + forecast.list[2].weather[0].icon + ".png";
+        iconUrl9 = url + forecast.list[3].weather[0].icon + ".png";
+
+        forecastDate3 = forecast.list[1].dt_txt.substring(8, 10) + '/' + forecast.list[1].dt_txt.substring(5, 7) + '/' + forecast.list[1].dt_txt.substring(0, 4) + ' ' +  forecast.list[1].dt_txt.substring(11, 13);
+        forecastDate6 = forecast.list[2].dt_txt.substring(8, 10) + '/' + forecast.list[2].dt_txt.substring(5, 7) + '/' + forecast.list[2].dt_txt.substring(0, 4) + ' ' +  forecast.list[2].dt_txt.substring(11, 13);
+        forecastDate9 = forecast.list[3].dt_txt.substring(8, 10) + '/' + forecast.list[3].dt_txt.substring(5, 7) + '/' + forecast.list[3].dt_txt.substring(0, 4) + ' ' +  forecast.list[3].dt_txt.substring(11, 13);
     }
     return (
       <div className="mt-5">
@@ -33,12 +49,36 @@ const Card = ({loadingData, showData, weather, forecast }) => {
                                 <p className="card-date">{date}</p>
                                 <h1 className="card-temp">{(weather.main.temp - 273.15).toFixed(1)}ºC</h1>
                                 <p className="card-desc"><img src={iconUrl} alt="icon"/>{weather.weather[0].description}</p>
-                                <img src="https://images.pexels.com/photos/11511728/pexels-photo-11511728.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="img-fluid rounded-start" alt="..."></img>
+                                <img src="https://images.pexels.com/photos/11511728/pexels-photo-11511728.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="img-fluid rounded-start" alt="..." />
                             </div>
                             <div className="col-md-8">
                                 <div className="card-body text-start mt-2">
-
+                                    <h5 className="card-test">Temperatura max: {(weather.main.temp_max - 273.15).toFixed(1)}ºC</h5>
+                                    <h5 className="card-test">Temperatura min: {(weather.main.temp_min - 273.15).toFixed(1)}ºC</h5>
+                                    <h5 className="card-test">Sensación térmica: {(weather.main.feels_like - 273.15).toFixed(1)}ºC</h5>
+                                    <h5 className="card-test">Humedad: {(weather.main.humidity)}%</h5>
+                                    <h5 className="card-test">Viento {(weather.wind.speed)} m/s</h5>
                                 </div>
+                                <hr/>
+                                <div className="row mt-4">
+                                    <div className="col">
+                                        <p>{forecastDate3}h</p>
+                                        <p className="description"><img src={iconUrl3} alt="icon"/>{forecast.list[1].weather[0].description}</p>
+                                        <p className="temp">{(forecast.list[1].main.temp - 273.15).toFixed(1)}ºC</p>
+                                    </div>
+                                    <div className="col">
+                                        <p>{forecastDate6}h</p>
+                                        <p className="description"><img src={iconUrl6} alt="icon"/>{forecast.list[2].weather[0].description}</p>
+                                        <p className="temp">{(forecast.list[2].main.temp - 273.15).toFixed(1)}ºC</p>
+                                    </div>
+                                    <div className="col">
+                                        <p>{forecastDate9}h</p>
+                                        <p className="description"><img src={iconUrl9} alt="icon"/>{forecast.list[3].weather[0].description}</p>
+                                        <p className="temp">{(forecast.list[3].main.temp - 273.15).toFixed(1)}ºC</p>
+                                    </div>
+                                </div>
+
+
                             </div>
                         </div>
                     </div>
--- src/components/Form.js
+++ src/components/Form.js
@@ -17,7 +17,7 @@ const Form = ({newLocation}) => {
             <form onSubmit={onSubmit}>
                 <div className='input-group mb-3 mx-auto'>
                     <input type='text' className='form-control'  placeholder='Ciudad'  onChange={(e) =>setCity(e.target.value)} />
-                    <button className='btn btn-primary input-group-text' type='submit'>Buscar</button>
+                    <button className='btn btn-primary-dark text-light border-light input-group-text' type='submit'>Buscar</button>
                 </div>
             </form>
         </div>
